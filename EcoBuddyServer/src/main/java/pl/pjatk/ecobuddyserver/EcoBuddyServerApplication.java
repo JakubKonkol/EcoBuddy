f6f966b7c30ba245199582abc24604a69e831613
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-01 22:06:07 +0200

    initial commit

---
+++ EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/EcoBuddyServerApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.ecobuddyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoBuddyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoBuddyServerApplication.class, args);
+	}
+
+}
---
+++ EcoBuddyServer/src/test/java/pl/pjatk/ecobuddyserver/EcoBuddyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.ecobuddyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoBuddyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
