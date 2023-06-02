e9ee4fa7974a5f3ef0e79e1048d1db04023e5923
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-02 23:51:58 +0200

    lets set up devices!!

--- EcoBuddyApp/src/app/components/eco-home-view/eco-home-view.component.ts
+++ EcoBuddyApp/src/app/components/eco-home-view/eco-home-view.component.ts
@@ -9,8 +9,27 @@ import {Device} from "../../shared/Device";
 })
 export class EcoHomeViewComponent implements OnInit{
   devices: Device[] = [];
+  yourDevices: Device[] = [];
+  shouldModalBeActive: boolean = false;
+  selectedDevice: Device | undefined;
   ngOnInit(): void {
     this.devices = new DeviceLoader().devices;
   }
 
+  selectDevice(device: Device) {
+    this.selectedDevice = device;
+    this.shouldModalBeActive = true;
+  }
+  addDevice() {
+    if(this.selectedDevice === undefined) return;
+    this.yourDevices.push(this.selectedDevice);
+    this.selectedDevice = undefined;
+    this.shouldModalBeActive = false;
+
+
+  }
+
+  removeDevice(device: Device) {
+    this.yourDevices.splice(this.yourDevices.indexOf(device), 1);
+  }
 }
