f998fd647ac9ef82a98356837a5855a1e649c922
IgorPodsiadlo <s24394@pjwstk.edu.pl>
2023-06-02 19:53:26 +0200

    setup project

---
+++ EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/model/Event.java
@@ -0,0 +1,74 @@
+package pl.pjatk.ecobuddyserver.model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table()
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String description;
+    @Column
+    private String location;
+    @Column
+    private LocalDateTime date;
+    @Column
+    private String img;
+
+    public Event() {
+
+    }
+
+    public Event(Long id, String description, String location, LocalDateTime date, String img) {
+        this.id = id;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.img = img;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
---
+++ EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/service/EventService.java
@@ -0,0 +1,46 @@
+package pl.pjatk.ecobuddyserver.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.pjatk.ecobuddyserver.model.Event;
+import pl.pjatk.ecobuddyserver.repository.EventRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventService {
+ EventRepository eventRepository;
+
+ @Autowired
+public void EventService(EventRepository eventRepository){this.eventRepository = eventRepository;}
+
+
+    public Event createEvent(Event event){ return eventRepository.save(event);}
+
+    public List<Event> getAllEvent(Event event) {return eventRepository.findAll();}
+
+
+public Event getEvent(Long id){
+    Optional<Event> event = eventRepository.findById(id);
+    if(event.isPresent()){
+        return event.get();
+    }
+    throw new IllegalArgumentException();
+}
+public void deleteEvent(Long id){eventRepository.deleteById(id);}
+
+    public Event updateEvent(Long id, Event updatedEvent){
+     Event eventToUpdate = getEvent(id);
+     if (updatedEvent.getDate() != null){
+         eventToUpdate.setDate(updatedEvent.getDate());
+     }
+     if(updatedEvent.getDescription() != null){
+         eventToUpdate.setDescription(updatedEvent.getDescription());
+     }
+     if(updatedEvent.getImg() != null){
+         eventToUpdate.setImg(updatedEvent.getImg());
+     }
+     return  eventRepository.save(eventToUpdate);
+    }
+}
