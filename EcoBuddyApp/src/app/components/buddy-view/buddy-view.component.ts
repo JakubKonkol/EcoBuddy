27feef4393bc7e2d01dd0d840d72d6e0c3137875
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-02 20:52:01 +0200

    buddy component changes

--- EcoBuddyApp/src/app/components/buddy-view/buddy-view.component.ts
+++ EcoBuddyApp/src/app/components/buddy-view/buddy-view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Task} from 'src/app/shared/Task';
+import {EPriority} from "../../shared/EPriority";
+import {EStatus} from "../../shared/EStatus";
 
 @Component({
   selector: 'app-buddy-view',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./buddy-view.component.css']
 })
 export class BuddyViewComponent {
+  tasks: Task[] = [
+    new Task(1, "Task 1", EPriority.HIGH, EStatus.TODO),
+    new Task(2, "Task 2", EPriority.MEDIUM, EStatus.IN_PROGRESS),
+    new Task(3, "Task 3", EPriority.LOW, EStatus.DONE)
+  ]
 
 }
