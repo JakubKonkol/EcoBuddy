da09378ff1b8fda7d697bdf77e1fa5cea47e72f9
s25663 <s25663@pjwstk.edu.pl>
2023-06-02 21:56:19 +0200

    endpointy_w_taskcontroller

--- EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/controller/EventController.java
+++ EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/controller/EventController.java
@@ -7,24 +7,24 @@ import pl.pjatk.ecobuddyserver.model.Event;
 import pl.pjatk.ecobuddyserver.service.EventService;
 
 @RestController
-@RequestMapping("/assets/")
+@RequestMapping("/assets")
 public class EventController {
     EventService eventService;
 
     @Autowired
     public EventController(EventService eventService){this.eventService = eventService;}
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Event>  createEvent(@RequestBody Event event){
         return  ResponseEntity.ok(eventService.createEvent(event));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/delete")
     public ResponseEntity<String> deleteEvent(@RequestParam("id") Long id){
         eventService.deleteEvent(id);
         return ResponseEntity.ok("ok");
     }
-    @PutMapping("{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<Event> updateEvent (@RequestBody Event event, @PathVariable("id") Long id){
        return  ResponseEntity.ok((eventService.updateEvent(id,event)));
     }
