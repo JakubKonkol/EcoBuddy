4fa8fbe9e71aa012ae75727f844d5ea0a41d7f66
IgorPodsiadlo <s24394@pjwstk.edu.pl>
2023-06-02 20:33:59 +0200

    event

--- EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/controller/EventController.java
+++ EcoBuddyServer/src/main/java/pl/pjatk/ecobuddyserver/controller/EventController.java
@@ -6,14 +6,24 @@ import org.springframework.web.bind.annotation.*;
 import pl.pjatk.ecobuddyserver.model.Event;
 import pl.pjatk.ecobuddyserver.service.EventService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/assets/")
+@RequestMapping("/event/")
 public class EventController {
     EventService eventService;
 
     @Autowired
     public EventController(EventService eventService){this.eventService = eventService;}
+@GetMapping
+public ResponseEntity<List<Event>> getAllEvent(){
+        return ResponseEntity.ok(eventService.getAllEvent());
+}
 
+@GetMapping("/{id}")
+public ResponseEntity<Event> getEvent(@PathVariable("id") Long id){
+        return ResponseEntity.ok(eventService.getEvent(id));
+}
     @PostMapping
     public ResponseEntity<Event>  createEvent(@RequestBody Event event){
         return  ResponseEntity.ok(eventService.createEvent(event));
