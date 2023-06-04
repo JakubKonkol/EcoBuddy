a746ac80f40ada2148c59dc139f179ba60a0ac2b
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-04 13:05:46 +0200

    I LOVE LATINAS

--- EcoBuddyApp/src/app/components/buddy-view/buddy-view.component.ts
+++ EcoBuddyApp/src/app/components/buddy-view/buddy-view.component.ts
@@ -14,4 +14,8 @@ export class BuddyViewComponent {
     new Task(2, "Wyrzuc smieci za sasiada", EPriority.MEDIUM, EStatus.TODO),
     new Task(3, "Posprzataj ocean", EPriority.HIGH, EStatus.TODO)
   ]
+
+  getTotalMoney():number {
+    return Math.floor(Math.random() * 10000) + 10000;
+  }
 }
