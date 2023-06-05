16202d11d19b3f8acdd7e24b392c8ff1b27c189d
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-05 12:37:13 +0200

    Event service

--- EcoBuddyApp/src/app/services/event.service.ts
+++ EcoBuddyApp/src/app/services/event.service.ts
@@ -2,15 +2,27 @@ import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
 import {IEvent} from "../shared/IEvent";
 import {HttpClient} from "@angular/common/http";
+import {AppSettings} from "../other/AppSettings";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-  private baseUrl:string = 'http://localhost:8080/event';
+  private baseUrl:string = AppSettings.API_ENDPOINT + 'event';
   constructor(private http: HttpClient) { }
   public getEvents(): Observable<IEvent[]>{
-    return this.http.get<IEvent[]>(this.baseUrl+'/getAll');
-
+    return this.http.get<IEvent[]>(this.baseUrl+'/findAll');
+  }
+  public getEvent(id: number): Observable<IEvent>{
+    return this.http.get<IEvent>(this.baseUrl + '/findById/' + id);
+  }
+  public updateEvent(event: IEvent): Observable<IEvent>{
+    return this.http.put<IEvent>(this.baseUrl + '/update/' + event.id, event);
+  }
+  public addEvent(event: IEvent): Observable<IEvent>{
+    return this.http.post<IEvent>(this.baseUrl + '/addEvent', event);
+  }
+  public deleteEvent(event: IEvent): Observable<IEvent>{
+    return this.http.delete<IEvent>(this.baseUrl + '/delete/' + event.id);
   }
 }
